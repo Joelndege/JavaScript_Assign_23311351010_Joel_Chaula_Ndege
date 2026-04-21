4e4c745945b41ed43169961d71215b45975a2f36
Joel Ndege <ndegejoel2000@gmail.com>
2026-04-21 21:11:52 +0200

    My 3rd commit

---
+++ phase-1/prog-12.js
@@ -0,0 +1,43 @@
+console.log("=".repeat(55));
+console.log("JAVASCRIPT PROGRAMMING LABORATORY - VERIFIED EXECUTION");
+console.log("STUDENT: JOEL L CHAULA NDEGE");
+console.log("ID: 23311351010");
+console.log("PROGRAM: [XX] - Nested conditional statements");
+console.log("EXECUTION DATE: " + new Date().toLocaleString());
+console.log("SESSION TOKEN: " + Math.random().toString(36).substring(2, 15).toUpperCase());
+console.log("VERIFICATION HASH: " + btoa("23311351010" + Date.now()).substring(0,
+12));
+console.log("=".repeat(55));
+// Program 12: Nested conditional statements
+
+let age = parseInt(prompt("Enter your age:"));
+
+let hasLicense = prompt(
+  "Do you have a driver's license? (yes/no)"
+).toLowerCase();
+
+if (age >= 18) {
+  console.log("You are old enough to drive.");
+
+  if (hasLicense === "yes") {
+    console.log("You can legally drive a car.");
+  } else if (hasLicense === "no") {
+    console.log("You need to get a license first.");
+  } else {
+    console.log("Invalid input for license.");
+  }
+
+} else if (age >= 16) {
+  console.log("You can get a learner's permit.");
+
+  if (hasLicense === "yes") {
+    console.log("You may drive with supervision.");
+  } else if (hasLicense === "no") {
+    console.log("Apply for a learner's permit.");
+  } else {
+    console.log("Invalid input for license.");
+  }
+
+} else {
+  console.log("You are too young to drive.");
+}
